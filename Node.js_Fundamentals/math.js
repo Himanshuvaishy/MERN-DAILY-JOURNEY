4017204769cc1ba491b59bf5bd97a14d9dcd6c6f
Himanshu <vaishhimanshu83170@gmail.com>
2025-08-19 00:48:16 +0530

    learn about node.js common module

---
+++ Node.js_Fundamentals/math.js
@@ -0,0 +1,17 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// const obj={
+// "name":"Himanshu"
+// }
+
+// module.exports = { add,obj};
+
+exports.sayHello = function(name) {
+    return `Hello, ${name}!`;
+};
