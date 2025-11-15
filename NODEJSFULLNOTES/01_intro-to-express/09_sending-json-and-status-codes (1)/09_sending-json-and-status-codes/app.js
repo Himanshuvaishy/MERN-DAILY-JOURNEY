cda26accb55e75e49288b3e2facaae60f8cef230
Himanshu vaishy <vaishhimanshu83170@gmail.com>
2025-11-15 08:58:56 +0530

    express middlewares and static files

--- NODEJSFULLNOTES/01_intro-to-express/09_sending-json-and-status-codes (1)/09_sending-json-and-status-codes/app.js
+++ NODEJSFULLNOTES/01_intro-to-express/09_sending-json-and-status-codes (1)/09_sending-json-and-status-codes/app.js
@@ -1,12 +1,10 @@
 import express from "express";
 
 const app = express();
-
-app.disable("x-powered-by");
 const port = 4000;
 
 app.get("/", (req, res) => {
-  res.end("Hello World! 😀");
+  res.status(301).json({ message: "Hello World!!" });
 });
 
 app.listen(port, () => {
